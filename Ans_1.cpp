b4193132488a57519377a142ee6ce8da4f3d59ec
Shakira <287145569+lilywinnie@users.noreply.github.com>
2026-07-19 20:24:25 +0600

    Create Ans_1.cpp

---
+++ Ans_1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int T;
+    cin>>T;
+    
+    while (T--){
+        
+        int N;
+        cin>>N;
+        
+        if(N%2==0)
+            cout<<N/2<<"\n";
+            
+        else
+            cout<<(N+1)/2<<"/n";
+    }
+}
